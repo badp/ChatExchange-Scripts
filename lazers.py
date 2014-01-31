878815f9473340b7bd290a380e80ad37a8110599
badp <badpazzword@gmail.com>
2014-01-31 23:56:02 +0100

    Appease the pylint and pep8 gods
    Improve the style of the code base. Spaces are not evil!

--- lazers.py
+++ lazers.py
@@ -1,37 +1,45 @@
 #requires https://pypi.python.org/pypi/websocket-client/
 import websocket
-import threading
-import json,os,sys,getpass
+import json
+import os
+import getpass
 from ChatExchange.SEChatWrapper import *
 from BeautifulSoup import *
 
-if("ChatExchangeU" in os.environ):
-  username=os.environ["ChatExchangeU"]
+if "ChatExchangeU" in os.environ:
+  username = os.environ["ChatExchangeU"]
 else:
   print "Username: "
-  username=raw_input()
-if("ChatExchangeP" in os.environ):
-  password=os.environ["ChatExchangeP"]
+  username = raw_input()
+if "ChatExchangeP" in os.environ:
+  password = os.environ["ChatExchangeP"]
 else:
-  password=getpass.getpass("Password: ")
+  password = getpass.getpass("Password: ")
 
+wrap = SEChatWrapper("SE")
+wrap.login(username, password)
 
-wrap=SEChatWrapper("SE")
-wrap.login(username,password)
+url = "https://github.com/Manishearth/ChatExchange-Scripts/blob/master/lazers.py"
+prefix = "[ [LAZERS v2](%s) ]" % url
 
 
 def handlepost(data):
-  a=json.loads(json.loads(data)['data'])
-  tags=a['tags']
-  tagtext=" ".join(["[tag:"+t+"]" for t in tags])
-  b=BeautifulSoup(a['body']).find('a',{'class':'question-hyperlink'})
-  s="[ [LAZERS v2](https://github.com/Manishearth/ChatExchange-Scripts/blob/master/lazers.py) ]  [%s](%s) %s" % (" ".join(b.contents),"http://gaming.stackexchange.com"+b['href'],tagtext)
-  print s
-  wrap.sendMessage("35",s)
+  a = json.loads(json.loads(data)['data'])
+  tags = a['tags']
+  link = BeautifulSoup(a['body']).find('a', {'class': 'question-hyperlink'})
+  msg = "{prefix} [{title}]({url}) %s".format(
+    prefix=prefix,
+    title=" ".join(link.contents),
+    url="http://gaming.stackexchange.com" + link['href'],
+    tags=" ".join("[tag:%s]" % t for t in tags)
+  )
+  print msg
+  wrap.sendMessage("35", msg)
 
-ws = websocket.create_connection("ws://sockets.ny.stackexchange.com/")
-ws.send("41-questions-newest")
-while True:
-  a=ws.recv()
-  if(a!= None and a!= ""):
-    handlepost(a)
+if __name__ == "__init__":
+  ws = websocket.create_connection("ws://sockets.ny.stackexchange.com/")
+  ws.send("41-questions-newest")
+  while True:
+    a = ws.recv()
+    if a is not None and a != "":
+      handlepost(a)
--- lazersonebox.py
+++ lazersonebox.py
@@ -1,41 +1,42 @@
 #requires https://pypi.python.org/pypi/websocket-client/
 import websocket
-import threading
-import json,os,sys,getpass
+import json
+import os
+import getpass
 from ChatExchange.SEChatWrapper import *
 from BeautifulSoup import *
 
-if("ChatExchangeU" in os.environ):
-  username=os.environ["ChatExchangeU"]
+if "ChatExchangeU" in os.environ:
+  username = os.environ["ChatExchangeU"]
 else:
   print "Username: "
-  username=raw_input()
-if("ChatExchangeP" in os.environ):
-  password=os.environ["ChatExchangeP"]
+  username = raw_input()
+if "ChatExchangeP" in os.environ:
+  password = os.environ["ChatExchangeP"]
 else:
-  password=getpass.getpass("Password: ")
+  password = getpass.getpass("Password: ")
 
+wrap = SEChatWrapper("SE")
+wrap.login(username, password)
 
-wrap=SEChatWrapper("SE")
-wrap.login(username,password)
+url = "https://github.com/Manishearth/ChatExchange-Scripts/blob/master/lazers.py"
+prefix = "[ [LAZERS v2](%s) ]" % url
 
 
 def handlepost(data):
   try:
-    a=json.loads(json.loads(data)['data'])
+    a = json.loads(json.loads(data)['data'])
   except:
     return False
-  tags=a['tags']
-  tagtext=" ".join(["[tag:"+t+"]" for t in tags])
-  b=BeautifulSoup(a['body']).find('a',{'class':'question-hyperlink'})
-  s="http://gaming.stackexchange.com"+b['href']
-  print s
-  wrap.sendMessage("35",s)
+  link = BeautifulSoup(a['body']).find('a', {'class': 'question-hyperlink'})
+  msg = "http://gaming.stackexchange.com" + link['href']
+  print msg
+  wrap.sendMessage("35", msg)
 
-ws = websocket.create_connection("ws://sockets.ny.stackexchange.com/")
-ws.send("41-questions-newest")
-while True:
-  a=ws.recv()
-  if(a!= None and a!= ""):
-    print a
-    handlepost(a)
+if __name__ == "__init__":
+  ws = websocket.create_connection("ws://sockets.ny.stackexchange.com/")
+  ws.send("41-questions-newest")
+  while True:
+    a = ws.recv()
+    if a is not None and a != "":
+      handlepost(a)
